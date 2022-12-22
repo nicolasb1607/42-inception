9d8e0d52febd83e44ab7588c885b22f26e2a1c48
Nicolas <nicolasburat1607@gmail.com>
2022-12-22 15:26:51 +0100

    FIX wordpress fonctionne

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -12,22 +12,23 @@
 sleep 10 
 
 
-wp core download --local=nl_NL
-
-
-if ! wp core is-installed; then
-	wp core install --url=$DB_HOST \
-					--title=Inception \
-					--admin_user=$DB_USER \
-					--admin_password=$DB_PASSWORD \
-					--admin_email=$ADMIN_MAIL
-fi
+wp core download --locale=nl_NL --allow-root --path='/var/www/html/wordpress/'
 
 wp config create --allow-root \
 	--dbname=$DB_NAME \
 	--dbuser=$DB_USER \
 	--dbpass=$DB_PASSWORD \
-	--dbhost="mariadb" --path="/var/www/html/wordpress/"
+	--dbhost="mariadb" --path='/var/www/html/wordpress/'
+
+wp core install --url=$DB_HOST \
+				--title=Inception \
+				--admin_user=$DB_USER \
+				--admin_password=$DB_PASSWORD \
+				--admin_email=$ADMIN_MAIL \
+				--allow-root \
+				--path='/var/www/html/wordpress/'
+
+
 
 mkdir -p /run/php/php7.3
 
