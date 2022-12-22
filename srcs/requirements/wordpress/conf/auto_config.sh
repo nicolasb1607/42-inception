ef906a32bc40a54652ca52e12e8ac4f209cd0c60
Nicolas <nicolasburat1607@gmail.com>
2022-12-22 18:50:32 +0100

    ADD user with privileges only on wpdb

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -2,19 +2,19 @@
 
 sleep 10 
 
-wp core download --locale=en_EN --allow-root --path='/var/www/html/wordpress/'
+wp core download --locale=fr_FR --allow-root --path='/var/www/html/wordpress/'
 
 wp config create --allow-root \
 	--dbname=$DB_NAME \
-	--dbuser=$DB_USER \
-	--dbpass=$DB_PASSWORD \
+	--dbuser=$DB_ADMIN \
+	--dbpass=$DB_ADMIN_PASSWORD \
 	--dbhost="mariadb" \
 	--path='/var/www/html/wordpress/'
 
 wp core install --url=$DB_HOST \
-				--title=Inception \
-				--admin_user=$DB_USER \
-				--admin_password=$DB_PASSWORD \
+				--title=$WP_TITLE \
+				--admin_user=$DB_ADMIN \
+				--admin_password=$DB_ADMIN_PASSWORD \
 				--admin_email=$ADMIN_MAIL \
 				--allow-root \
 				--path='/var/www/html/wordpress/'
